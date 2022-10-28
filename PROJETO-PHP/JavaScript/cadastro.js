269fe1d889759920f1d5ada51292ee38af472c57
Davyd Edivaldo <davydedivaldo@gmail.com>
2022-10-27 21:56:05 -0300

    alterações

--- PROJETO-PHP/JavaScript/cadastro.js
+++ PROJETO-PHP/JavaScript/cadastro.js
@@ -1,14 +1,1 @@
 
-const animaCad = document.getElementById('animação-cadastro');
-const clicado = document.getElementById('login');
-document.getElementById('login').addEventListener('click', function(){
-    clicado.classList.toggle('clicado');
-    if(clicado.classList == 'clicado'){
-        animaCad.classList.remove('esconder');
-        animaCad.classList.remove('esconder2');
-        animaCad.classList.add('cadastro');
-    }else if(clicado != ('clicado')){
-        animaCad.classList.add('esconder2');
-        animaCad.classList.remove('cadastro');
-    }
-});
